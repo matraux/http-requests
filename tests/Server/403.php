1564886194f2c6cea527fe56b12a2a8124a49504
Daniel Žůrek <info@danielzurek.cz>
2025-09-05 12:32:07 +0000

    Feat: Tests HttpServer

---
+++ tests/Server/403.php
@@ -0,0 +1,4 @@
+<?php declare(strict_types=1);
+
+http_response_code(403);
+header('Content-Type: text/plain; charset=UTF-8');
---
+++ tests/Server/HttpServer.php
@@ -0,0 +1,99 @@
+<?php declare(strict_types=1);
+
+namespace Matraux\HttpRequestsTest\Server;
+
+use Throwable;
+use RuntimeException;
+use Symfony\Component\Process\Process;
+
+final class HttpServer
+{
+
+	protected Process $process
+	{
+		get => $this->process ??= new Process([
+			PHP_BINARY,
+			'-S',
+			sprintf('%s:%d', $this->host, $this->port),
+			'-t',
+			__DIR__,
+		]);
+	}
+
+	public private(set) string $host = '127.0.0.1';
+
+	public int $port
+	{
+		get => $this->port ??= (function(): int {
+			if(!$socket = stream_socket_server(sprintf('tcp://%s:0', $this->host), $code, $message)) {
+				throw new RuntimeException(
+					is_string($message) ? $message : 'Unknown socket server error',
+					is_numeric($code) ? (int) $code : 0
+				);
+			}
+
+			if(!$authority = stream_socket_get_name($socket, false)) {
+				throw new RuntimeException('Failed to obtain authority');
+			}
+
+			fclose($socket);
+
+			if (!preg_match('/:(\d+)$/', $authority, $match)) {
+				throw new RuntimeException('Failed to obtain port from authority.');
+			}
+
+			return (int) $match[1];
+		})();
+	}
+
+	public string $url
+	{
+		get => $this->url ??= sprintf('http://%s:%d', $this->host, $this->port);
+	}
+
+	protected function __construct()
+	{
+	}
+
+	public static function create(): static
+	{
+		return new static();
+	}
+
+	public function __destruct()
+	{
+		try {
+			$this->stop();
+		} catch (Throwable $th) {
+
+		}
+	}
+
+	public function start(): static
+	{
+		if($this->process->isRunning()) {
+			return $this;
+		}
+
+		$this->process->start();
+		usleep(100 * 1000);
+
+		return $this;
+	}
+
+	public function stop(): static
+	{
+		if(!$this->process->isRunning()) {
+			return $this;
+		}
+
+		try {
+			$this->process->stop();
+		} catch (Throwable $th) {
+
+		}
+
+		return $this;
+	}
+
+}
